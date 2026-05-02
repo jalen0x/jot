52c23113937fe99e924e74d5036986c19b43eb7d
Jalen Yan <jalenx.me@outlook.com>
2026-05-03 01:33:49 +0800

    feat: add account creation UI

---
+++ app/controllers/accounts_controller.rb
@@ -0,0 +1,72 @@
+class AccountsController < ApplicationController
+  before_action :authenticate_user!
+
+  # GET /accounts
+  def index
+    authorize Account
+    @accounts = policy_scope(Account).kept.where(parent_account_id: nil).order(:display_order, :name)
+  end
+
+  # GET /accounts/new
+  def new
+    @account = current_user.accounts.build(default_account_attributes)
+    authorize @account
+  end
+
+  # POST /accounts
+  def create
+    authorize Account
+
+    result = AccountCreator.new.create_account(
+      user: current_user,
+      attributes: account_attributes,
+      opening_balance_cents: opening_balance_cents
+    )
+
+    if result.created?
+      redirect_to accounts_path, notice: "Account created."
+    else
+      @account = result.account
+      render :new, status: :unprocessable_content
+    end
+  end
+
+  private
+
+  def account_attributes
+    account_params.except(:opening_balance_cents).merge(display_order: next_display_order)
+  end
+
+  def account_params
+    params.expect(account: [
+      :name,
+      :account_category,
+      :account_structure,
+      :icon_key,
+      :color_hex,
+      :currency_code,
+      :opening_balance_cents,
+      :comment
+    ])
+  end
+
+  def opening_balance_cents
+    account_params[:opening_balance_cents].to_i
+  end
+
+  def next_display_order
+    current_user.accounts.kept.where(parent_account_id: nil).maximum(:display_order).to_i + 1
+  end
+
+  def default_account_attributes
+    {
+      account_category: :cash,
+      account_structure: :single_account,
+      icon_key: 1,
+      color_hex: "22C55E",
+      currency_code: "USD",
+      balance_cents: 0,
+      display_order: next_display_order
+    }
+  end
+end
---
+++ test/integration/accounts_test.rb
@@ -0,0 +1,63 @@
+require "test_helper"
+
+class AccountsTest < ActionDispatch::IntegrationTest
+  test "requires authentication" do
+    get accounts_path
+
+    assert_redirected_to new_user_session_path
+  end
+
+  test "lists only current user accounts" do
+    user = create(:user)
+    other_user = create(:user)
+    own_account = create_account(user: user, name: "Checking")
+    create_account(user: other_user, name: "Other Checking")
+
+    sign_in user
+    get accounts_path
+
+    assert_response :success
+    assert_select "h1", text: /accounts/i
+    assert_select "li", text: /#{own_account.name}/i
+    assert_select "li", text: /Other Checking/i, count: 0
+  end
+
+  test "creates an account for current user" do
+    user = create(:user)
+    sign_in user
+
+    post accounts_path, params: {
+      account: {
+        name: "Cash",
+        account_category: "cash",
+        account_structure: "single_account",
+        icon_key: "1",
+        color_hex: "22C55E",
+        currency_code: "USD",
+        opening_balance_cents: "1234",
+        comment: "Wallet"
+      }
+    }
+
+    account = user.accounts.sole
+    assert_redirected_to accounts_path
+    assert_equal "Cash", account.name
+    assert_equal 1234, account.balance_cents
+  end
+
+  private
+
+  def create_account(user:, name:)
+    Account.create!(
+      user: user,
+      name: name,
+      account_category: :checking_account,
+      account_structure: :single_account,
+      icon_key: 1,
+      color_hex: "2563EB",
+      currency_code: "USD",
+      balance_cents: 0,
+      display_order: 1
+    )
+  end
+end
