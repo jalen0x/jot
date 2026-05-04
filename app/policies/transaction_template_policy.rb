18d8b901b62ede60b732d305d5ae3a7c38c59eb3
Jalen Yan <jalenx.me@outlook.com>
2026-05-04 13:57:00 +0800

    feat: add transaction template show api

--- app/policies/transaction_template_policy.rb
+++ app/policies/transaction_template_policy.rb
@@ -1,5 +1,6 @@
 class TransactionTemplatePolicy < ApplicationPolicy
   def index? = user.present?
+  def show? = user.present? && record.user_id == user.id
   def create? = user.present?
   def update? = user.present? && record.user_id == user.id
   def destroy? = user.present? && record.user_id == user.id
