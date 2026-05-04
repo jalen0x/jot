bd614d42544150af8dbae677d6a6ca60a6e13cd1
Jalen Yan <jalenx.me@outlook.com>
2026-05-05 06:06:22 +0800

    feat: add transaction category html editing

--- test/integration/transaction_categories_test.rb
+++ test/integration/transaction_categories_test.rb
@@ -42,6 +42,59 @@ class TransactionCategoriesTest < ActionDispatch::IntegrationTest
     assert_predicate category, :income?
   end
 
+  test "updates a category for current user" do
+    user = create(:user)
+    category = create_category(user: user, name: "Groceries")
+    sign_in user
+
+    get edit_transaction_category_path(category)
+    assert_response :success
+    assert_select "h1", text: /edit category/i
+    assert_select "input#transaction_category_hidden"
+
+    patch transaction_category_path(category), params: {
+      transaction_category: {
+        name: "Restaurants",
+        category_type: "expense",
+        icon_key: "3",
+        color_hex: "#22c55e",
+        hidden: "1",
+        comment: "Dining out"
+      }
+    }
+
+    assert_redirected_to transaction_categories_path
+    category.reload
+    assert_equal "Restaurants", category.name
+    assert_equal "expense", category.category_type
+    assert_equal 3, category.icon_key
+    assert_equal "22C55E", category.color_hex
+    assert_predicate category, :hidden?
+    assert_equal "Dining out", category.comment
+  end
+
+  test "does not update another user's category" do
+    user = create(:user)
+    other_user = create(:user)
+    category = create_category(user: other_user, name: "Other Groceries")
+    sign_in user
+
+    patch transaction_category_path(category), params: {
+      transaction_category: {
+        name: "Changed",
+        category_type: "expense",
+        icon_key: "3",
+        color_hex: "22C55E",
+        hidden: "1",
+        comment: "Changed"
+      }
+    }
+
+    assert_response :not_found
+    assert_equal "Other Groceries", category.reload.name
+    refute_predicate category, :hidden?
+  end
+
   private
 
   def create_category(user:, name:)
