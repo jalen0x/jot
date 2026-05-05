d51f2e7a0cb943d05acfbdacde4f4bf1b4f614b3
Jalen Yan <jalenx.me@outlook.com>
2026-05-05 09:21:49 +0800

    Add HTML transaction category parent selection

--- app/controllers/transaction_categories_controller.rb
+++ app/controllers/transaction_categories_controller.rb
@@ -5,22 +5,30 @@ class TransactionCategoriesController < ApplicationController
   def index
     authorize TransactionCategory
     @transaction_categories = policy_scope(TransactionCategory).kept.where(parent_category_id: nil).order(:category_type, :display_order, :name)
+    @sub_categories_by_parent_id = policy_scope(TransactionCategory).kept
+      .where(parent_category_id: @transaction_categories.select(:id))
+      .order(:category_type, :display_order, :name)
+      .group_by(&:parent_category_id)
   end
 
   # GET /transaction_categories/new
   def new
     @transaction_category = current_user.transaction_categories.build(default_category_attributes)
+    @parent_category_options = parent_category_options
     authorize @transaction_category
   end
 
   # POST /transaction_categories
   def create
     authorize TransactionCategory
-    @transaction_category = current_user.transaction_categories.build(category_attributes)
+    @transaction_category = current_user.transaction_categories.build(category_params.except(:parent_category_id))
+    @transaction_category.parent_category = parent_category_for(@transaction_category)
+    @transaction_category.display_order = next_display_order(@transaction_category.parent_category)
 
-    if @transaction_category.save
+    if @transaction_category.errors.empty? && @transaction_category.save
       redirect_to transaction_categories_path, notice: "Category created."
     else
+      @parent_category_options = parent_category_options
       render :new, status: :unprocessable_content
     end
   end
@@ -28,6 +36,7 @@ class TransactionCategoriesController < ApplicationController
   # GET /transaction_categories/:id/edit
   def edit
     @transaction_category = scoped_category
+    @parent_category_options = parent_category_options(excluding: @transaction_category)
     authorize @transaction_category
   end
 
@@ -41,6 +50,7 @@ class TransactionCategoriesController < ApplicationController
       redirect_to transaction_categories_path, notice: "Category updated."
     else
       @transaction_category = result.category
+      @parent_category_options = parent_category_options(excluding: result.category)
       render :edit, status: :unprocessable_content
     end
   end
@@ -56,24 +66,36 @@ class TransactionCategoriesController < ApplicationController
 
   private
 
-  def category_attributes
-    category_params.merge(display_order: next_display_order)
-  end
-
   def category_params
-    params.expect(transaction_category: [ :name, :category_type, :icon_key, :color_hex, :comment ])
+    @category_params ||= params.expect(transaction_category: [ :name, :category_type, :parent_category_id, :icon_key, :color_hex, :comment ])
   end
 
   def category_update_params
-    params.expect(transaction_category: [ :name, :category_type, :icon_key, :color_hex, :hidden, :comment ])
+    params.expect(transaction_category: [ :name, :category_type, :parent_category_id, :icon_key, :color_hex, :hidden, :comment ])
+  end
+
+  def parent_category_for(category)
+    parent_category_id = category_params[:parent_category_id]
+    return if parent_category_id.blank?
+
+    current_user.transaction_categories.kept.find(TransactionCategory.decode_prefix_id(parent_category_id) || parent_category_id)
+  rescue ActiveRecord::RecordNotFound
+    category.errors.add(:parent_category, "is unavailable")
+    nil
   end
 
   def scoped_category
     policy_scope(TransactionCategory).kept.find(params[:id])
   end
 
-  def next_display_order
-    current_user.transaction_categories.kept.where(parent_category_id: nil).maximum(:display_order).to_i + 1
+  def next_display_order(parent_category = nil)
+    current_user.transaction_categories.kept.where(parent_category: parent_category).maximum(:display_order).to_i + 1
+  end
+
+  def parent_category_options(excluding: nil)
+    scope = current_user.transaction_categories.kept.order(:category_type, :display_order, :name)
+    scope = scope.where.not(id: excluding.id) if excluding
+    scope
   end
 
   def default_category_attributes
